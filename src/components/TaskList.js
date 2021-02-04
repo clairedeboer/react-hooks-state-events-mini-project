d0813e13cbc7b548e645aa04bce1c52c2949126e
clairedeboer <claire.deboer@gmail.com>
2021-02-03 22:10:29 -0500

    Done.

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,10 +1,24 @@
 import React from "react";
+import Task from "./Task"; 
 
-function TaskList() {
+const TaskList = ({ tasks }) => {
+  const [tasksArray, setTasksArray] = React.useState(tasks)
+
+  const removeTask = (textToBeDeleted) => {
+    setTasksArray(tasksArray.filter((task) => textToBeDeleted !== task.text))
+  }
+  //how do we know which task was removed? 
+
+  const taskList = tasksArray.map((task) => {
+    return <Task 
+      key={task.text}
+      text={task.text}
+      category={task.category}
+      removeTask={removeTask}
+    />
+  })
   return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
+    <div className="tasks">{taskList}</div>
   );
 }
 
